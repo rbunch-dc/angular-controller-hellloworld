7dd2e618f8772bbd60a95dd6d041dfeda3ceacb5
Robert Bunch <robert.bunch@tinroofsoftware.com>
2015-11-09 11:59:48 -0500

    Hello world project - init

---
+++ controller.js
@@ -0,0 +1,19 @@
+var controllers = angular.module('myApp', []); 
+controllers.controller('appController',appController);
+
+function appController($scope){
+
+	$scope.name = "World!";
+
+	$scope.students = ['Will', 'Peter', 'Griffin', 'Stuart', 'Stephen', 'Chance', 'Andrew', 'Oliver', 'Freddy', 'Yohsuke'];
+
+
+	$scope.changeName = function(){
+		$scope.name = "World (from button)";
+	}
+
+	$scope.addStudent = function(){
+		$scope.students.push($scope.newName);
+	}
+
+}
